2cf1e27d93b42a514547876d28ef32f02c32b2ac
Emanuel Ott <emanuelott@Mac.fritz.box>
2025-11-02 19:22:33 +0100

    Simplify extension structure to match fan control
    - Removed hasUI flag (not needed)
    - Removed onSources callback
    - Simplified to basic extension structure matching fan-control
    - Changed module export to match working extensions

--- beocreate/beo-extensions/ir-remote-control/index.js
+++ beocreate/beo-extensions/ir-remote-control/index.js
@@ -1,26 +1,17 @@
-var ir_remote_control = (function() {
-
-    // Extension metadata
+var extensions = (function() {
     var extensions = {};
     
     extensions["ir-remote-control"] = {
         displayName: "IR Remote Control",
         description: "Control your audio receiver via infrared remote",
         version: "1.0.0",
-        hidden: false,
-        hasUI: true
-    };
-
-    // Load UI when extension is added to sources  
-    extensions["ir-remote-control"].onSources = function(data) {
-        console.log("IR Remote Control extension loaded");
+        hidden: false
     };
 
     return extensions;
-
 })();
 
 if (typeof module !== 'undefined' && module.exports) {
-    module.exports = ir_remote_control;
+    module.exports = extensions;
 }
 
